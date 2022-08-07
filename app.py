19af5654bfd031acf6ff4dfb53a2f7e4ba215f19
Huzeyfe Canbaz <104665324+huzeyfecanbaz@users.noreply.github.com>
2022-08-07 16:47:02 -0600

    All issues solved

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ import scraping
 app=Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"]="mongodb://localhost:27017/mars_app"
+app.config['MONGO_URI']= "mongodb://localhost:27017/mars_app"
 mongo=PyMongo(app)
 
 #define the route for html page
 @app.route("/")
 def index():
-    mars=mongo.db.mars.find_one
-    return render_template('index.html',mars=mars)
+    mars=mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
 
 #define route for scrape
 @app.route("/scrape")
--- scraping.py
+++ scraping.py
@@ -1,9 +1,10 @@
 # Import Splinter, BeautifulSoup, and Pandas
+
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
-import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
+import datetime as dt
 
 
 def scrape_all():
@@ -19,8 +20,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-    }
+        "last_modified": dt.datetime.now()}
 
     # Stop webdriver and return data
     browser.quit()
@@ -31,7 +31,7 @@ def mars_news(browser):
 
     # Scrape Mars News
     # Visit the mars nasa news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    url = 'https://redplanetscience.com'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -57,7 +57,7 @@ def mars_news(browser):
 
 def featured_image(browser):
     # Visit URL
-    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full image button
@@ -71,13 +71,13 @@ def featured_image(browser):
     # Add try/except for error handling
     try:
         # Find the relative image url
-        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get("src")
 
     except AttributeError:
         return None
 
     # Use the base url to create an absolute url
-    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
     return img_url
 
@@ -85,7 +85,7 @@ def mars_facts():
     # Add try/except for error handling
     try:
         # Use 'read_html' to scrape the facts table into a dataframe
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
     except BaseException:
         return None
@@ -95,7 +95,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html()
 
 if __name__ == "__main__":
 
